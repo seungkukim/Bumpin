0b759237b3a0019fa16343c300dbf9f882bc84d3
HeeWooLee <gmldn00@gmail.com>
2022-01-07 16:33:54 +0900

    feat: 3 Butons in MainActivity - 각 버튼은 새로운 Activity로 연결 - Map 버튼은 Google Map API과 연결되어 있음 - Login과 Register 버튼은 미구현

--- app/src/main/java/com/example/bumpin/MainActivity.java
+++ app/src/main/java/com/example/bumpin/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.bumpin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Map
+        Button btn_map = findViewById(R.id.btn_map);
+        btn_map.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+        });
+
+        // Login
+        Button btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        // Register
+        Button btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
