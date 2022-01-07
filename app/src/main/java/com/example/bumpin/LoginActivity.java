485aa343271c8dbf0f0c2ae7e3208b638edfc352
Seungku Kim <seungkukim@kaist.ac.kr>
2022-01-07 17:09:44 +0900

    login register screen created

--- app/src/main/java/com/example/bumpin/LoginActivity.java
+++ app/src/main/java/com/example/bumpin/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.bumpin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Login
+        Button btn_login = findViewById(R.id.loginButton);
+        btn_login.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
+            startActivity(intent);
+        });
+
+        // Register
+        Button btn_register = findViewById(R.id.registerButton);
+        btn_register.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/bumpin/RegisterActivity.java
+++ app/src/main/java/com/example/bumpin/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.bumpin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Register
+        Button registerNewButton = findViewById(R.id.registerNewButton);
+        registerNewButton.setOnClickListener(view -> {
+            Intent intent = new Intent(RegisterActivity.this, MapsActivity.class);
+            startActivity(intent);
+        });
     }
+
 }
