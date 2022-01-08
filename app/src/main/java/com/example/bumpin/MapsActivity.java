c82069882f1b6cb005a4e4f2fa5c495a14b05558
Seungku Kim <seungkukim@kaist.ac.kr>
2022-01-08 18:17:23 +0900

    map added tracking trips

--- app/src/main/java/com/example/bumpin/MapsActivity.java
+++ app/src/main/java/com/example/bumpin/MapsActivity.java
@@ -1,6 +1,10 @@
 package com.example.bumpin;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
@@ -13,13 +17,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.WeakHashMap;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private Boolean btnClicked = false;
+    private String tripName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+//        Button newBtn = findViewById(R.id.newBtn);
+//        Button listBtn = findViewById(R.id.listBtn);
+//        Button friendBtn = findViewById(R.id.friendBtn);
+//
+//        newBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
     }
 
     /**
@@ -66,19 +86,107 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //            }
 //        );
 
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//            @Override
+//            public void onMapClick(@NonNull LatLng latLng) {
+//                MarkerOptions markerOptions = new MarkerOptions();
+//                markerOptions.position(latLng);
+//                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+//                mMap.clear();
+//
+//                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+//
+//                mMap.addMarker(markerOptions);
+//            }
+//        });
+
+
+
+        Button newBtn = findViewById(R.id.newBtn);
+        Button listBtn = findViewById(R.id.listBtn);
+        Button friendBtn = findViewById(R.id.friendBtn);
+
+        ArrayList<MarkerOptions> trip;
+        PolylineOptions polylineOptions;
+
+        newBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapClick(@NonNull LatLng latLng) {
-                MarkerOptions markerOptions = new MarkerOptions();
-                markerOptions.position(latLng);
-                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-                mMap.clear();
+            public void onClick(View view) {
+                if(btnClicked == true){
+                    btnClicked = false;
+                    newBtn.setText("Add");
+                    return;
+                }
+                btnClicked = true;
+                newBtn.setText("Stop");
 
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+                View v = getLayoutInflater().inflate(R.layout.edit_box, null, false);
+                builder.setView(v);
+                final AlertDialog dialog = builder.create();
 
-                mMap.addMarker(markerOptions);
+                Button addTripBtn = (Button) v.findViewById(R.id.addTripBtn);
+                final EditText editTextTripName = (EditText) v.findViewById(R.id.editTextTripName);
+
+                addTripBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        tripName = editTextTripName.getText().toString();
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
+
+                HashMap<String, ArrayList<MarkerOptions>> map = new HashMap<>();
+                ArrayList<MarkerOptions> trip = new ArrayList<MarkerOptions>();
+                map.put(tripName, trip);
+                PolylineOptions polylineOptions = new PolylineOptions();
+
+                mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                    @Override
+                    public void onMapLongClick(@NonNull LatLng latLng) {
+                        if(btnClicked == false) {
+                            return;
+                        }
+                        else{
+                            MarkerOptions mo = new MarkerOptions()
+                                    .position(latLng)
+                                    .draggable(true)
+                                    .title("Your marker title")
+                                    .snippet("Your marker snippet");
+                            mMap.addMarker(mo);
+                            polylineOptions.add(latLng);
+                            mMap.addPolyline(polylineOptions);
+                            trip.add(mo);
+                        }
+                    }
+                });
+            }
+
+        });
+
+        listBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
+
+
+//        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+//            @Override
+//            public void onMapLongClick(@NonNull LatLng latLng) {
+//                mMap.addMarker(new MarkerOptions()
+//                    .position(latLng)
+//                    .draggable(true)
+//                    .title("Your marker title")
+//                    .snippet("Your marker snippet"));
+//            }
+//        });
+
+
+
 
     }
 }
